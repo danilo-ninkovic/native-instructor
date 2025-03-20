fa3dfb6fb94a158504e1aa46fbe5df67b4e512d6
Danilo Ninkovic <ninkovicdanilo@hotmail.com>
2025-03-20 13:24:07 +0100

    Install RN Paper

--- frontend/App.js
+++ frontend/App.js
@@ -2,18 +2,22 @@ import { StatusBar } from "expo-status-bar"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { StyleSheet } from "react-native"
+import { Provider as PaperProvider } from "react-native-paper"
 import LoginScreen from "./screens/LoginScreen"
+import theme from "./utils/PaperTheme"
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={theme}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </>
   )
 }
