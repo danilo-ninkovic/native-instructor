8e54e142546790051bfaad047c2da731e6441f3b
Danilo Ninkovic <ninkovicdanilo@hotmail.com>
2025-03-20 12:42:50 +0100

    Random image in LoginScreen

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,28 +1,46 @@
-import { StyleSheet, Text, View } from "react-native"
-import React, { useState } from "react"
-import CustomTextInput from "../components/basics/CustomTextInput"
+import { StyleSheet, Text, View, Image } from "react-native"
+import React, { useState, useFocusEffect, useCallback } from "react"
+import { getRandomImage } from "../utils/Utils"
 
 export default function LoginScreen() {
-  const [text, setText] = useState("")
-  const [password, setPassword] = useState("")
+  const [randomImage, setRandomImage] = useState(null)
+
+  useFocusEffect(
+    useCallback(() => {
+      setRandomImage(getRandomImage)
+    }, [])
+  )
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-      <CustomTextInput
-        label="Username"
-        value={text}
-        onChangeText={setText}
-        placeholder="Enter your username"
-      />
-      <CustomTextInput
-        label="Password"
-        isPassword={true}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Enter your password"
-      />
+    <View style={styles.container}>
+      <Image source={randomImage} style={styles.backgroundImage} />
+      <View style={styles.overlay}>
+        <Text style={styles.title}> Welcome</Text>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.3)",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 20,
+  },
+})
